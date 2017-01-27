b7ccec43c1cc310dc34677b518c9b3ab1f3d91d4
FatimaZahra <fz.eddaghour@lekiosque.fr>
2017-01-27 01:35:02 +0000

    [UI Tests] Setting up test environment

---
+++ src/test/java/androidtests/Config.java
@@ -0,0 +1,41 @@
+package androidtests;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by FatimaZahra on 27/01/2017.
+ */
+public class Config {
+
+    File appDir = new File("..//LeKiosqueAndroid/lekioskAndroid/app/build/outputs/apk/");
+    File app = new File(appDir, "app-lekiosk-preprod.apk");
+
+    static AndroidDriver driver;
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+
+    public Config() {
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("app", app.getAbsolutePath());
+
+        URL url = null;
+
+        try {
+            url = new URL("http://127.0.0.1:4723/wd/hub");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        if (driver == null) {
+            driver = new AndroidDriver(url, capabilities);
+            System.out.printf(driver.toString());
+        }
+    }
+
+    public AndroidDriver getDriver() {
+        return driver;
+    }
+}
---
+++ src/test/java/androidtests/SearchTest.java
@@ -0,0 +1,92 @@
+package androidtests;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//import io.appium.java_client.android.Connection;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+/**
+ * Created by FatimaZahra on 27/01/2017.
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class SearchTest {
+
+    static Config d = new Config();
+    WebDriverWait wait = new WebDriverWait(d.getDriver(), 10);
+
+    //Online mode
+
+    @Test
+    //Should not show results for an unexisting keyword
+    public void testASearchWithUnexistingKeyword() throws Exception {
+        wait.until(visibilityOfElementLocated(By.id("Recherche")));
+        d.getDriver().findElement(By.id("Recherche")).click();
+        d.getDriver().findElement(By.id("CDSearchUIText")).clear();
+        d.getDriver().findElement(By.id("CDSearchUIText")).sendKeys("XXXX" + "\n");
+        wait.until(visibilityOfElementLocated(By.id("CDSearchNoResultUIText")));
+        Assert.assertEquals("Aucun résultat", this.d.getDriver().findElement(By.id("CDSearchNoResultUIText")).getText());
+    }
+
+    @Test
+
+    //Should show result for titles close to the given keyword
+    public void testBSearchWithCloseKeyWord() throws Exception {
+        d.getDriver().findElement(By.id("Effacer la requête")).click();
+        d.getDriver().findElement(By.id("CDSearchUIText")).sendKeys("poin" + "\n");
+        wait.until(visibilityOfElementLocated(By.id("CDSearchItemUICover")));
+        d.getDriver().findElement(By.id("CDSearchItemUICover")).click();
+        wait.until(visibilityOfElementLocated(By.id("CDIssueTitleUILabel")));
+        Assert.assertEquals("Le Point", this.d.getDriver().findElement(By.id("CDIssueTitleUILabel")).getText());
+    }
+
+    @Test
+    //Should show results for an existing title
+    public void testCSearchForExistingTitle() throws Exception {
+        d.getDriver().findElement(By.id("CDMenuBackUIButtun")).click();
+        wait.until(visibilityOfElementLocated(By.id("Effacer la requête")));
+        d.getDriver().findElement(By.id("Effacer la requête")).click();
+        d.getDriver().findElement(By.id("CDSearchUIText")).sendKeys("Le Point" + "\n");
+        wait.until(visibilityOfElementLocated(By.id("CDSearchItemUICover")));
+        d.getDriver().findElement(By.id("CDSearchItemUICover")).click();
+        wait.until(visibilityOfElementLocated(By.id("CDIssueTitleUILabel")));
+        Assert.assertEquals("Le Point", this.d.getDriver().findElement(By.id("CDIssueTitleUILabel")).getText());
+    }
+
+    //OffLine mode
+/*
+    @Test
+    //No results found in offLine mode.
+    public void testDSearchInOffLineMode() throws Exception {
+        //set connection to OFF
+        d.getDriver().setConnection(Connection.AIRPLANE);
+        //test Start
+        d.getDriver().findElement(By.id("CDMenuBackUIButtun")).click();
+        wait.until(visibilityOfElementLocated(By.id("Effacer la requête")));
+        d.getDriver().findElement(By.id("Effacer la requête")).click();
+        d.getDriver().findElement(By.id("CDSearchUIText")).sendKeys("OffLineText" + "\n");
+        wait.until(visibilityOfElementLocated(By.id("CDSearchTextInternetProblem")));
+        Assert.assertEquals("Problème de connexion", this.d.getDriver().findElement(By.id("CDSearchTextInternetProblem")).getText());
+
+    }
+    */
+    //TODO search in cache
+    //Search in cache test case could not be tested because the functionality is not implemented, nothing is saved in cache
+
+    @AfterClass
+    public static void setConnectionBack() {
+       // d.getDriver().setConnection(Connection.ALL);
+    }
+
+    public static void tearDown() {
+        d.getDriver().quit();
+    }
+
+}
